a275c1d3eca226102649181dd4cabf2c6f9e50ff
koteg <fancar61@mail.ru>
2023-06-07 13:08:44 +0300

    gta2 game

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" type="module"></script>
+</head>
+<body>
+<img src="./img/wasted.png" class="wasted" alt="">
+<div class="score-wrap">
+    <span>Score: </span>
+    <span id="Score">0</span>
+</div>
+<div class="bg-wrap">
+    <div class="left-bg"></div>
+    <div class="center-bg"></div>
+    <div class="right-bg"></div>
+</div>
+<canvas></canvas>
+</body>
+</html>
